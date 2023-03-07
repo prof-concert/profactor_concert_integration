d2c92511db1ea7e9b59b81120db0fa8a8f99a42d
kdeshp <kdeshp@profactor.at>
2023-03-07 16:12:14 +0100

    created a docker to perform detection and classification using yolov7

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # profactor_concert_integration
 This repository contains code that integrates profactor stack to the complete project.
+
+cacert.crt and http_proxy_signing_ca.crt are required for building the image inside profactor network.
+Please comment the copying and update of ca-certificated being done by this files outside the profactor network.
--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-nvidia-docker run --rm -it --gpus all \
+docker run --rm -it --gpus all \
+ --runtime=nvidia \
  --env="DISPLAY=$DISPLAY" \
+ --env="ROS_MASTER_URI=http://192.168.1.178:11311" \
  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
  --volume="$HOME/.ssh:/home/user/.ssh:ro" \
- --volume="$HOME/profactor_concert_integration/ros_src:/home/user/concert_ws/src:ro" \
+ --volume="$HOME/profactor_concert_integration/ros_src:/home/user/concert_ws/src:rw" \
  --name concert_description \
- prof-concert/concert_prof_inter:latest \
- x-terminal-emulator
+ --network docker_bridge_cam \
+ --ip 192.168.1.179 \
+ prof-concert/concert_prof_inter:latest 
+ #x-terminal-emulator
